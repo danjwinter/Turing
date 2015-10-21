d8845cf62696bfa94f97d988bdb4e9c2fb5c132c
Dan Winter <dan.j.winter@gmail.com>
2015-10-20 21:10:07 -0600

    passed number convert

--- test_braille_letters.rb
+++ test_braille_letters.rb
@@ -14,7 +14,7 @@ class NightWriterTest < Minitest::Test
   def test_it_can_read_file
     night = NightWriter.new
     ARGV << "sample.txt"
-    assert_equal night.reader.read, "this is your first sample."
+    assert_equal night.reader.read, "this is your first sample. 768 lkj 2932 ld"
   end
 
   def test_letters_convert_to_braille
@@ -77,13 +77,13 @@ class NightWriterTest < Minitest::Test
     new_string = night.add_number_trigger("there are 90 dollars and 10 bananas")
     assert_equal "there are #90 dollars and #10 bananas", new_string
   end
-end
 
-  # def test_it_converts_numbers
-  #
-  # end
-  # def test_it_can_chunk_40_characters
-  #
-  #   night = FileRe
-  #   assert_equal night.chunk_from_alphabet, ""
-  # end
+  def test_it_converts_numbers
+  night = NightWriter.new
+  num_trigger_with_string = night.add_number_trigger("911")
+  night.convert_digit_to_braille(num_trigger_with_string)
+  assert_equal night.top, ".0.00.0."
+  assert_equal night.middle, ".00....."
+  assert_equal night.bottom, "00......"
+  end
+end
